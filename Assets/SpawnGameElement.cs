01065031055315c92cd7fdd6a08cdb6dcd7a21e9
anminh0412 <73033384+anminh0412@users.noreply.github.com>
2023-02-14 08:45:16 +0700

    a

--- Assets/SpawnGameElement.cs
+++ Assets/SpawnGameElement.cs
@@ -7,10 +7,11 @@ public class SpawnGameElement : MonoBehaviour
 {
     public void Spawn(int _col, int _row,float _distant, int _elements, GameObject _go, Transform _spawnPosition)
     {
-        for(int i = 1; i <= _elements; i++) 
-        {
-            GameObject go = Instantiate(_go, new Vector3(0, 0, 0), Quaternion.identity);
-            go.transform.position = _spawnPosition.position + new Vector3(transform.position.x - _col / 2f*i, transform.position.y + _row / 2f*i, 0f) + new Vector3(_col * _distant, -_row * _distant, 0f);
-        }
+        for (int row = 0; row < _row; row++)
+            for (int column = 0; column < _col; column++)
+            {
+                GameObject newTile = Instantiate(_go);
+                newTile.transform.position = transform.position + new Vector3(transform.position.x - column / 2f, transform.position.y + _row / 2f, 0f) + new Vector3(column * _distant, -row * _distant, 0f);
+            }
     }
 }
